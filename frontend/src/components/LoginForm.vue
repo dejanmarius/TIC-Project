522090345b6fe2e811236e99234de48a774ecf87
dejanmarius <mariusdejan6@gmail.com>
2025-02-02 13:15:22 +0200

    Added v-snackbar for alertsand  implemented http status handling for login and register

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -2,56 +2,49 @@
   <div class="login-container">
     <v-form ref="form" v-model="formIsValid" @submit.prevent="login">
       <v-card class="mx-auto pa-6" max-width="448" style="min-width: 400px; width: 100%;">
+
         <v-card-title class="text-h3 text-center font-weight-bold">Sign In</v-card-title>
 
         <div class="text-subtitle-1 text-large-emphasis">Email</div>
-        <v-text-field 
-          v-model="email" 
-          density="compact" 
-          placeholder="Email address"
-          prepend-inner-icon="mdi-email-outline" 
-          variant="outlined" 
-          rounded="lg" 
-          bg-color="#eeeeee" 
-          color="#3c4ebe"
+        <v-text-field v-model="email" density="compact" placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline" variant="outlined" rounded="lg" bg-color="#eeeeee" color="#3c4ebe"
           :rules="emailRules">
         </v-text-field>
 
         <div class="text-subtitle-1 text-large-emphasis">Password</div>
-        <v-text-field 
-          v-model="password" 
-          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="visible ? 'text' : 'password'" 
-          density="compact" 
-          placeholder="Enter your password"
-          prepend-inner-icon="mdi-lock-outline" 
-          variant="outlined" 
-          rounded="lg" 
-          @click:append-inner="visible = !visible"
-          bg-color="#eeeeee" 
-          color="#3c4ebe" 
-          :rules="passwordRules">
+        <v-text-field v-model="password" :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="visible ? 'text' : 'password'" density="compact" placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline" variant="outlined" rounded="lg" @click:append-inner="visible = !visible"
+          bg-color="#eeeeee" color="#3c4ebe" :rules="passwordRules">
         </v-text-field>
 
         <v-btn class="mb-5 mt-2" color="#3c4ebe" size="large" block @click="submitForm" rounded="lg">
           Sign In
         </v-btn>
         <v-card-text class="text-center">
-          <a @click="goToRegister" class=" text-decoration-none" href="#" style="color: #3c4ebe">
+          <a @click.prevent="goToRegister" class="text-decoration-none" href="#"
+            style="color: #3c4ebe; cursor: pointer;">
             No account? Create your RateFlix account
             <v-icon icon="mdi-chevron-right"></v-icon>
           </a>
-        </v-card-text>
-      </v-card>
 
+          <v-snackbar v-model="snackbar" color="red darken-2" timeout="3000" elevation="2" location="top">
+            {{ errorMessage }}
+            <template v-slot:actions>
+              <v-btn icon @click="snackbar = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </template>
+          </v-snackbar>
+        </v-card-text>
+
+      </v-card>
     </v-form>
   </div>
 </template>
 
 
 <script>
-
-
 export default {
   data() {
     return {
@@ -59,6 +52,8 @@ export default {
       password: '',
       visible: false,
       formIsValid: false,
+      snackbar: false,
+      errorMessage: '',
       emailRules: [
         v => !!v || 'Email is required',
       ],
@@ -96,16 +91,31 @@ export default {
           const data = await response.json();
           console.log('Login successful', data);
 
+          this.$store.commit('setAuthState', {
+            isAuthenticated: true,
+            userRole: data.role,
+            userName: data.name,
+            userId: data.userId
+          });
+
           if (data.role === 'admin') {
             this.$router.push('/adminDashboard');
           } else {
             this.$router.push('/');
           }
         } else {
-          console.error('Login failed:', response.status);
+          if (response.status === 401) {
+            this.errorMessage = 'The combination between email and password is invalid.';
+          } else if (response.status === 500) {
+            this.errorMessage = 'Server error. Please try again later.';
+          } else {
+            this.errorMessage = 'An unexpected error occurred.';
+          }
+          this.snackbar = true;
         }
       } catch (error) {
-        console.error('An error occurred:', error);
+        this.errorMessage = 'Network error. Please check your connection.';
+        this.snackbar = true;
       }
     },
 
--- frontend/src/components/RegisterForm.vue
+++ frontend/src/components/RegisterForm.vue
@@ -1,57 +1,27 @@
 <template>
   <div class="register-container">
-  <v-card
-    class=" pa-6"
-    max-width="448"
-    style="min-width: 300px; width: 100%;" 
-  >
-  <v-card-title class="text-h3 text-center font-weight-bold">Create account</v-card-title>
-      <v-text-field
-        v-model="name"
-        label="Name"
-        density="compact"
-        variant="outlined" 
-        rounded="lg"
-        bg-color="#eeeeee" 
-        color="#3c4ebe"
-        prepend-inner-icon="mdi-account"
-      ></v-text-field>
+    <v-form ref="form" v-model="formIsValid" @submit.prevent="register">
+      <v-card class=" pa-6" max-width="448" style="min-width: 300px; width: 100%;">
+        <v-card-title class="text-h3 text-center font-weight-bold">Create account</v-card-title>
 
-      <v-text-field
-        label="Email"
-        v-model="email" 
-        density="compact" 
-        prepend-inner-icon="mdi-email-outline" 
-        variant="outlined" 
-        rounded="lg" 
-        bg-color="#eeeeee" 
-        color="#3c4ebe"
-      ></v-text-field>
+        <v-text-field v-model="username" label="Name" density="compact" variant="outlined" rounded="lg"
+          bg-color="#eeeeee" color="#3c4ebe" prepend-inner-icon="mdi-account" :rules="nameRules"></v-text-field>
 
-      <v-text-field
-        v-model="password"
-        label="Password"
-        density="compact"
-        variant="outlined"
-        prepend-inner-icon="mdi-lock-outline" 
-        rounded="lg" 
-        bg-color="#eeeeee"
-        color="#3c4ebe"
-      ></v-text-field>
+        <v-text-field label="Email" v-model="email" density="compact" prepend-inner-icon="mdi-email-outline"
+          variant="outlined" rounded="lg" bg-color="#eeeeee" color="#3c4ebe" :rules="emailRules"></v-text-field>
 
-      <v-text-field
-        v-model="confirmPassword"
-        label="Re-enter password"
-        variant="outlined"
-        density="compact"
-        prepend-inner-icon="mdi-lock-outline" 
-        rounded="lg" 
-        bg-color="#eeeeee"
-        color="#3c4ebe"
-        
-      ></v-text-field>
+        <v-text-field v-model="password" label="Password" density="compact" variant="outlined"
+          prepend-inner-icon="mdi-lock-outline" rounded="lg" bg-color="#eeeeee" color="#3c4ebe" :rules="passwordRules"
+          :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'" :type="visible ? 'text' : 'password'"
+          @click:append-inner="visible = !visible"></v-text-field>
 
-    <v-btn class="mb-5 mt-2" color="#3c4ebe" size="large" block @click="register" rounded="lg">
+        <v-text-field v-model="confirmPassword" label="Re-enter password" variant="outlined" density="compact"
+          prepend-inner-icon="mdi-lock-outline" rounded="lg" bg-color="#eeeeee" color="#3c4ebe"
+          :rules="confirmPasswordRules" :append-inner-icon="confirmVisible ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append-inner="confirmVisible = !confirmVisible"
+          :type="confirmVisible ? 'text' : 'password'"></v-text-field>
+
+        <v-btn class="mb-5 mt-2" color="#3c4ebe" size="large" block @click="submitForm" rounded="lg">
           Create your account
         </v-btn>
         <v-card-text class="text-center">
@@ -59,10 +29,28 @@
             Already have an account? Sign in
             <v-icon icon="mdi-chevron-right"></v-icon>
           </a>
+          <v-snackbar v-model="snackbar" :color="snackbarColor" timeout="3000" elevation="2" location="top">
+            {{ errorMessage }}
+            <template v-slot:actions>
+              <v-btn icon @click="snackbar = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </template>
+          </v-snackbar>
         </v-card-text>
 
-  </v-card>
-</div>
+      </v-card>
+    </v-form>
+
+    <v-snackbar v-model="snackbar" color="green" timeout="3000" elevation="2" location="top">
+      {{ errorMessage }}
+      <template v-slot:actions>
+        <v-btn icon @click="snackbar = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -70,22 +58,56 @@
 export default {
   data() {
     return {
-      name: '',
+      username: '',
       email: '',
-      password: ''
+      password: '',
+      confirmPassword: '',
+      visible: false,
+      confirmVisible: false,
+      formIsValid: false,
+      snackbar: false,
+      errorMessage: '',
+      snackbarColor: '',
+      emailRules: [
+        v => !!v || 'Email is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 6 || 'Password must be at least 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password confirmation is required',
+        v => v === this.password || 'Passwords must match'
+      ],
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length >= 3 || 'Name must be at least 3 characters'
+      ]
     };
   },
   methods: {
-    async register() {
 
+    async submitForm() {
+      if (this.formIsValid) {
+        this.register(); // Daca formularul este valid, executa functia de inregistrare
+      } else {
+        this.$refs.form.validate(); // Daca formularul nu este valid, il validezi pentru a afisa mesajele de eroare
+      }
+    },
+    async register() {
+      if (this.password !== this.confirmPassword) {
+        alert('Passwords do not match');
+        return;
+      }
       try {
         const response = await fetch('http://localhost:3000/register', {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json' 
+            'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            name: this.name,
+            username: this.username,
             email: this.email,
             password: this.password
           })
@@ -93,18 +115,37 @@ export default {
 
         const data = await response.json();
         console.log(data);
-        
+
         if (response.ok) {
-          this.$router.push('/login');
-        } else {
-          alert('Registration failed: ' + data.message || 'Something went wrong');
+          this.snackbarColor = 'green';
+          this.errorMessage = 'Your account has been successfully created. You will be redirected to the login page shortly.';
+          this.snackbar = true;
+
+          setTimeout(() => {
+            this.$router.push('/login');
+          }, 5000);
+        } else if (response.status === 409) {
+          this.snackbarColor = 'red';
+          this.snackbar = true;
+          this.errorMessage = 'An account with this email already exists.';
+        } else if (response.status === 500) {
+          this.snackbarColor = 'red';
+          this.snackbar = true;
+          this.errorMessage = 'An unexpected error occurred on the server. Please try again later.';
+        }
+        else {
+        this.snackbarColor = 'red';
+        this.snackbar = true;
+        this.errorMessage = `Unexpected error occurred Please try again later.`;
+        console.log(response.status);
         }
       } catch (error) {
-        console.error(error);
-        alert('Registration failed');
+        this.snackbarColor = 'red';
+        this.snackbar = true;
+        this.errorMessage = 'Network error. Please check your connection.';
       }
     },
-    
+
     goTologin() {
       this.$router.push('/login');
     }
@@ -112,17 +153,20 @@ export default {
 };
 </script>
 
-<style >
+<style>
 .register-container {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  
+
 }
+
 .v-btn {
   font-size: 16px;
 }
 
-
+.v-text-field {
+  margin-bottom: 5px;
+}
 </style>
